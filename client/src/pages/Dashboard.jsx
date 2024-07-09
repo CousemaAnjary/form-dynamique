489f20a80f21ddbf4b8ac5d8a610d1f1b928fe5c
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-09 18:17:51 +0300

    chore: Update react-router-dom to version 6.24.1

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+export default function Dashboard() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Dashboard
+            </h1>
+        </>
+    )
+}
