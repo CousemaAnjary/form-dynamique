f167326a8dfd2d6445f6d84d0e6161f21e104d33
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-09 18:58:40 +0300

    chore: Update dependencies and add Avatar component

---
+++ client/src/components/dashboard/Navbar.jsx
@@ -0,0 +1,67 @@
+import { Button } from '../ui/button';
+
+import { Link } from 'react-router-dom';
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+import { LogOut, Settings, User, MessageSquareMore, Bell } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
+
+
+export default function Navbar() {
+    return (
+        <nav className="bg-white p-4 shadow sticky top-0 z-50">
+            <div className="container-fluid mx-20 flex justify-between items-center">
+                <div className="flex items-center space-x-4">
+                    <Link to="#" className="text-2xl font-bold font-serif text-black">FormFlow</Link>
+                </div>
+
+                <div className="flex items-center space-x-4">
+                    {/* <Link to="/friendzy/chat">
+                        <Button variant="outline" className="p-2 shadow-sm">
+                            <MessageSquareMore size={18} />
+                        </Button>
+                    </Link>
+                    <Link to="#">
+                        <Button variant="outline" className="p-2 shadow-sm relative">
+                            <Bell size={18} />
+                        </Button>
+                    </Link> */}
+                    <DropdownMenu>
+
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="relative h-10 w-10 rounded-full">
+                                <Avatar className="h-10 w-10">
+                                    <AvatarImage src="" alt="avatar" />
+                                    <AvatarFallback className="bg-transparent">CN</AvatarFallback>
+                                </Avatar>
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent className="w-56" align="center" forceMount>
+                            <DropdownMenuLabel>Salut👋 Utilisateur</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <Link to="#">
+                                    <DropdownMenuItem>
+                                        <User className="mr-2 h-4 w-4" />
+                                        <span>Profile</span>
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link to="#">
+                                    <DropdownMenuItem>
+                                        <Settings className="mr-2 h-4 w-4" />
+                                        <span>Paramètres</span>
+                                    </DropdownMenuItem>
+                                </Link>
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <LogOut className="mr-2 h-4 w-4" />
+                                <span>Déconnexion</span>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/dashboard/Navbar";
+
 export default function Dashboard() {
     /**
      * ! STATE (état, données) de l'application
@@ -14,9 +16,23 @@ export default function Dashboard() {
      */
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                Dashboard
-            </h1>
+            <div className="min-h-screen w-full">
+                {/* Navbar */}
+                <Navbar />
+
+                <div className="flex flex-1 mt-10 h-full overflow-x-hidden">
+                    {/* Left Sidebar */}
+
+                    <main className={`flex-grow  transition-all duration-300 `}>
+                        {/* Contenu principal */}
+                    </main>
+
+                    <div className="flex-shrink-0 w-64 ml-20">
+                        {/* Right Sidebar */}
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
