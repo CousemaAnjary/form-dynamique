ee86b8501d9f65929b8ecabbb42272996a9576bf
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-09 18:11:41 +0300

    chore: Update Vite and React dependencies, configuration files, and remove unused CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </>
+
   )
 }
