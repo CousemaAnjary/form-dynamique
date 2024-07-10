fa5151d19fe81e667bebee42a7c33cb75e21d829
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-10 22:36:13 +0300

    chore: Mettre à jour la page de création de formulaire

--- client/src/pages/NewForm.jsx
+++ client/src/pages/NewForm.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Plus } from 'lucide-react';
 
 export default function NewForm() {
     return (
-        <div className="container mx-auto p-4">
-            <div className="flex justify-between items-center ">
-                <div className="flex items-center space-x-2">
-                    <div className="text-xl font-semibold">projet</div>
-                    <input
-                        type="text"
-                        className="border rounded px-4 py-2"
-                        placeholder="Titre"
-                        defaultValue="TEST"
-                    />
+        <>
+            <div className="shadow">
+                <div className="container-fluid mx-20 flex justify-between items-center h-20">
+                    <div className="flex items-center space-x-2">
+                        <h1 className="text-2xl font-semibold">projet</h1>
+                        <Input
+                            type="text"
+                            className="border rounded px-4 py-2"
+                            placeholder="Titre"
+                            defaultValue="Formulaire de contact"
+                            disabled
+                        />
+                    </div>
+                    <Button className="bg-blue-500 rounded-sm text-white">Sauvegarder</Button>
                 </div>
-                <Button className="bg-blue-500 text-white">SAUVEGARDER</Button>
             </div>
-            
-            <div className="border rounded p-4 text-center">
-                <p className="text-gray-500">
-                    Ce formulaire est actuellement vide.
-                    Vous pouvez ajouter des questions, notes, messages-guide ou autres champs en cliquant sur le signe « + » plus bas.
-                </p>
+            <div className="container mx-auto mt-10">
+                <div className="border rounded p-4 text-center relative">
+                    <p className="text-gray-500">
+                        Ce formulaire est actuellement vide.
+                        Vous pouvez ajouter des questions, notes, messages-guide ou autres champs en cliquant sur le signe « + » plus bas.
+                    </p>
+                    <Button className="absolute left-0 top-7 transform translate-x-[-50%] translate-y-[-50%]  rounded-full p-2 flex items-center justify-center bg-blue-500"  >
+                        <Plus size={24}   />
+                    </Button> 
+                </div>
             </div>
-        </div>
+        </>
     );
 }
