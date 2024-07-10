d97b6f80979832324d161bb89115dced32402621
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-10 22:37:06 +0300

    chore: Mettre à jour la page de création de formulaire

--- client/src/pages/NewForm.jsx
+++ client/src/pages/NewForm.jsx
@@ -10,13 +10,7 @@ export default function NewForm() {
                 <div className="container-fluid mx-20 flex justify-between items-center h-20">
                     <div className="flex items-center space-x-2">
                         <h1 className="text-2xl font-semibold">projet</h1>
-                        <Input
-                            type="text"
-                            className="border rounded px-4 py-2"
-                            placeholder="Titre"
-                            defaultValue="Formulaire de contact"
-                            disabled
-                        />
+                        <Input type="text" className="border rounded px-4 py-2" placeholder="Titre" defaultValue="Formulaire de contact" disabled />
                     </div>
                     <Button className="bg-blue-500 rounded-sm text-white">Sauvegarder</Button>
                 </div>
@@ -28,8 +22,8 @@ export default function NewForm() {
                         Vous pouvez ajouter des questions, notes, messages-guide ou autres champs en cliquant sur le signe « + » plus bas.
                     </p>
                     <Button className="absolute left-0 top-7 transform translate-x-[-50%] translate-y-[-50%]  rounded-full p-2 flex items-center justify-center bg-blue-500"  >
-                        <Plus size={24}   />
-                    </Button> 
+                        <Plus size={24} />
+                    </Button>
                 </div>
             </div>
         </>
