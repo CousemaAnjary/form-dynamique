7586e6b44567d535fc1cf8af5ae3f4f73fd227de
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-11 21:18:00 +0300

    chore: Update Question and Option models with fillable attributes

--- api/app/Models/Option.php
+++ api/app/Models/Option.php
@@ -5,15 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
+class Option extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'value',
         'label',
-        'type',
-        'required',
-        'position',
-        'project_id',
+        'question_id',
     ];
 }
