206c72d7a83727e7045aae40b71033485ef1fcb2
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-11 21:55:25 +0300

    chore: Ajouter la fonctionnalité de création de projet

--- client/src/services/projectService.js
+++ client/src/services/projectService.js
@@ -1,1 +1,13 @@
 import api from './apiConfig'
+
+// créer une nouvelle projet
+export const createProject = async (projectData) => {
+    try {
+        // envoyer les données du projet au serveur via une requête POST
+        const response = await api.post('/projects', projectData)
+        return response.data // retourner les données du projet
+
+    } catch (error) {
+        console.error('Erreur lors de la création du projet : ', error)
+    }
+}
