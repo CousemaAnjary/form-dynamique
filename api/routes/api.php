df0716ccf70c02a041bd56960c5e04500b276a45
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-11 23:42:41 +0300

    chore: Update API routes and ProjectDialog component for project creation functionality improvement

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('projects', [ProjectController::class, 'store']);
+Route::post('project', [ProjectController::class, 'store']);
 Route::get('projects', [ProjectController::class, 'index']);
--- client/src/services/projectService.js
+++ client/src/services/projectService.js
@@ -4,7 +4,7 @@ import api from './apiConfig'
 export const createProject = async (projectData) => {
     try {
         // envoyer les données du projet au serveur via une requête POST
-        const response = await api.post('/projects', projectData)
+        const response = await api.post('/project', projectData)
         return response.data // retourner les données du projet
 
     } catch (error) {
@@ -17,7 +17,7 @@ export const getProjects = async () => {
     try {
         // envoyer une requête GET au serveur pour récupérer la liste des projets
         const response = await api.get('/projects')
-        return response.data // retourner la liste des projets
+        return response.data.projects // retourner la liste des projets
 
     } catch (error) {
         console.error('Erreur lors de la récupération des projets : ', error)
