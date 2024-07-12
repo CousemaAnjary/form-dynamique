72ea2928bd893e166e137f296db9f9932034d97c
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-12 20:17:50 +0300

    chore: Add QuestionController and routes for questions in backend API

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Backend\ProjectController;
+use App\Http\Controllers\Api\Backend\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('project', [ProjectController::class, 'store']);
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('project/{id}', [ProjectController::class, 'show']);
+
+
+
+// Routes pour les questions
+Route::post('project/{projectId}/question', [QuestionController::class, 'store']);
+Route::get('project/{projectId}/questions', [QuestionController::class, 'index']);
+Route::get('question/{id}', [QuestionController::class, 'show']);
+Route::put('question/{id}', [QuestionController::class, 'update']);
+Route::delete('question/{id}', [QuestionController::class, 'destroy']);
