3e3c6318b553d4716798ce5ae4e61ade9150986f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-12 20:22:14 +0300

    chore: Ajouter la fonctionnalité de création de question dans le contrôleur QuestionController

--- api/app/Http/Controllers/Api/Backend/QuestionController.php
+++ api/app/Http/Controllers/Api/Backend/QuestionController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Api\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Question;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Backend\QuestionRequest;
 
 class QuestionController extends Controller
 {
-    //
+    public function store(QuestionRequest $request, int $projectId)
+    {
+        // Validation des données de la requête
+        $validated = $request->validated();
+
+        // Création de la question
+        $validated['project_id'] = $projectId;
+        $question = Question::create($validated);
+
+        return response()->json([
+            'question' => $question,
+            'message' => 'Question créée avec succès.'
+        ], 201);
+    }
 }
