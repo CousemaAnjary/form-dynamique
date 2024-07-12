0d86f38aa44a91d829ac1876a1a5c379ca28c178
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-12 20:25:36 +0300

    chore: Ajouter la fonctionnalité de création de question dans le service questionService.js

--- client/src/services/questionService.js
+++ client/src/services/questionService.js
@@ -1,1 +1,12 @@
 import api from './apiConfig'
+
+// créer une nouvelle question
+export const createQuestion = async (questionData) => {
+    try {
+        // envoyer une requête POST à l'API avec les données de la question
+        const response = await api.post(`/project/${questionData.project_id}/question`, questionData)
+        return response.data // retourner les données de la question
+    } catch (error) {
+        console.error('Erreur lors de la création de la question : ', error);
+    }
+}
